c2df40ca23f6342506cf6beb3e559f33bb56c869
Arslan Yousaf <devtechofficial90@gmail.com>
2025-06-11 20:11:36 +0500

    Refactor controller initialization to use lazy loading in init function

--- lib/helper/init_controllers.dart
+++ lib/helper/init_controllers.dart
@@ -3,6 +3,6 @@ import 'package:getx_app_learning/controllers/list_controller.dart';
 import 'package:getx_app_learning/controllers/tab_controller.dart';
 
 Future<void> init() async {
-  Get.put(TabControllerGet());
-  Get.put(ListController());
+  Get.lazyPut(() => TabControllerGet());
+  Get.lazyPut(() => ListController());
 }
