7fd2b1dd2350f83df1baf30e61056cbc7243cfd5
Arslan Yousaf <devtechofficial90@gmail.com>
2025-06-11 17:18:15 +0500

    Add total calculation and display for X and Y values in ThirdPage

--- lib/controllers/tab_controller.dart
+++ lib/controllers/tab_controller.dart
@@ -16,13 +16,19 @@ import 'package:get/get_state_manager/get_state_manager.dart';
 
 class TabControllerGet extends GetxController {
   int _x = 0;
-  RxInt _y = 0.obs; // Reactive variable using RxInt
+  RxInt _y = 0.obs;
+  RxInt _z = 0.obs;
+
+  // Reactive variable using RxInt
   // RxInt is a reactive integer that automatically updates the UI when changed
   // This is different from Flutter's built-in TabController, which is used for managing tabs in a TabBar.
   // Here, we are using GetX's state management to handle the counter logic.
   // The controller manages an internal counter (_x) and provides methods to increment and decrement it.
   int get x => _x;
   RxInt get y => _y;
+
+  RxInt get z => _z;
+
   // Getter for _x to access its value outside the controller
   // Getter for _y to access its reactive value outside the controller
   void increment() {
@@ -41,5 +47,10 @@ class TabControllerGet extends GetxController {
 
   void decreaseY() {
     _y.value--;
+  }
+
+  void total() {
+    _z.value = _x + _y.value;
+    print("Total: $_z");
   }
 }
